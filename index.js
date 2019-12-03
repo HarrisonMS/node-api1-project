35cddb079d66ec3a8f8e5d801936e98cc6c03f6b
HarrisonMS <54726103+HarrisonMS@users.noreply.github.com>
2019-12-02 16:14:37 -0800

    getting close

--- index.js
+++ index.js
@@ -95,6 +95,23 @@ server.put(`/users/:id`, (req, res) => {
     })
 })
 
+server.get("/users/:id", (req, res) => {
+    const { id } =req.params;
+    db.findById(id)
+        .then(user => {
+        console.log("user", user);
+        if(user) {
+            res.status(200).json(user);
+        }else {
+            res.status(404).json({error: "the user with that id doesnt exist"})
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: "couldn't find any info at that location"})
+    })
+})
+
 const port = 5000;
 server.listen(port, () => console.log(`\n API is running on port ${port}\n`)
 );
